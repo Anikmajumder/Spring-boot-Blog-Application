79b857c269197179c44462dc771cf488bbc85774
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2024-06-23 23:09:08 +0600

    spring boot project set up

---
+++ springboot-blog-rest-api-app/src/main/java/com/spingboot/blog/springboot_blog_rest_api_app/SpringbootBlogRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.spingboot.blog.springboot_blog_rest_api_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiAppApplication.class, args);
+	}
+
+}
---
+++ springboot-blog-rest-api-app/src/test/java/com/spingboot/blog/springboot_blog_rest_api_app/SpringbootBlogRestApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingboot.blog.springboot_blog_rest_api_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogRestApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
