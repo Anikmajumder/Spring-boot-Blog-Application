778dd8e32946505e427e576cb87f94fff4011aea
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2024-06-29 14:46:14 +0600

    get all posts

--- springboot-blog-rest-api-app/src/main/java/com/spingboot/blog/service/PostService.java
+++ springboot-blog-rest-api-app/src/main/java/com/spingboot/blog/service/PostService.java
@@ -1,8 +1,12 @@
 package com.spingboot.blog.service;
 
+import java.util.List;
+
 import com.spingboot.blog.payload.PostDto;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+   List<PostDto> getAllPost();
     
 } 
