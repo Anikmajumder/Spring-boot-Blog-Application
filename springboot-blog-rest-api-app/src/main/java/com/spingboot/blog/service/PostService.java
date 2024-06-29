70ae37f55a83aa2e7d073837521daa87251d0231
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2024-06-29 23:06:56 +0600

    Ordering in Sorting Api- ASC and DESC

--- springboot-blog-rest-api-app/src/main/java/com/spingboot/blog/service/PostService.java
+++ springboot-blog-rest-api-app/src/main/java/com/spingboot/blog/service/PostService.java
@@ -8,7 +8,7 @@ import com.spingboot.blog.payload.PostResponse;
 public interface PostService {
     PostDto createPost(PostDto postDto);
 
-    PostResponse getAllPost(int pageNo, int pageSize, String sortBy);
+    PostResponse getAllPost(int pageNo, int pageSize, String sortBy, String sortDir);
 
     PostDto getPostById(Long id);
 
