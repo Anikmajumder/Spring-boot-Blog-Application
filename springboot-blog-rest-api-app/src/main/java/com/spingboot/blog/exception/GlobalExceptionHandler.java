cdb7d1a9720b239a7415daefd24e1873f324af83
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2024-07-13 21:15:48 +0600

    Added Specific and Custom Exception

---
+++ springboot-blog-rest-api-app/src/main/java/com/spingboot/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.spingboot.blog.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.spingboot.blog.payload.ErrorDetails;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    //handle specific exception
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+    //global exception
+    
+}
